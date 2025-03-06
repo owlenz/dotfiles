c8ab7af53c29ce636fa350520ff9b4fc1920dfe6
Saif Al-Din Samy <seifowlen@proton.me>
2025-03-06 17:08:10 +0200

    git config and hyprland changes

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
-			-- popup_border_style = "rounded",
+			popup_border_style = "rounded",
 			filesystem = {
 				filtered_items = {
 					hide_dotfiles = false,
